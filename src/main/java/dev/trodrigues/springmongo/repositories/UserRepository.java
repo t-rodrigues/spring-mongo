6ec00839836944df00c1812394649a4c05687b05
Thiago Rodrigues <thiagor_@live.com>
2021-12-29 01:10:01 -0300

    feat: add User

---
+++ src/main/java/dev/trodrigues/springmongo/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package dev.trodrigues.springmongo.repositories;
+
+import dev.trodrigues.springmongo.models.entities.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
